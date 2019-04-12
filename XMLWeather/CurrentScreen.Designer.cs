28eb428b0cb01ac00fb3263bc033ba5af4de56fe
Sam Near <samnear17@gmail.com>
2019-04-12 09:30:55 -0400

    Add project files.

---
+++ XMLWeather/CurrentScreen.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace XMLWeather
+{
+    partial class CurrentScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CurrentScreen));
+            this.cityOutput = new System.Windows.Forms.Label();
+            this.maxLabel = new System.Windows.Forms.Label();
+            this.minLabel = new System.Windows.Forms.Label();
+            this.tempLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.conditionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cityOutput
+            // 
+            this.cityOutput.BackColor = System.Drawing.Color.Transparent;
+            this.cityOutput.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityOutput.ForeColor = System.Drawing.Color.White;
+            this.cityOutput.Location = new System.Drawing.Point(98, 17);
+            this.cityOutput.Name = "cityOutput";
+            this.cityOutput.Size = new System.Drawing.Size(72, 23);
+            this.cityOutput.TabIndex = 22;
+            this.cityOutput.Text = "City";
+            // 
+            // maxLabel
+            // 
+            this.maxLabel.AutoSize = true;
+            this.maxLabel.BackColor = System.Drawing.Color.Transparent;
+            this.maxLabel.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maxLabel.ForeColor = System.Drawing.Color.White;
+            this.maxLabel.Location = new System.Drawing.Point(137, 175);
+            this.maxLabel.Name = "maxLabel";
+            this.maxLabel.Size = new System.Drawing.Size(33, 15);
+            this.maxLabel.TabIndex = 31;
+            this.maxLabel.Text = "max";
+            // 
+            // minLabel
+            // 
+            this.minLabel.AutoSize = true;
+            this.minLabel.BackColor = System.Drawing.Color.Transparent;
+            this.minLabel.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minLabel.ForeColor = System.Drawing.Color.White;
+            this.minLabel.Location = new System.Drawing.Point(84, 175);
+            this.minLabel.Name = "minLabel";
+            this.minLabel.Size = new System.Drawing.Size(29, 15);
+            this.minLabel.TabIndex = 29;
+            this.minLabel.Text = "min";
+            // 
+            // tempLabel
+            // 
+            this.tempLabel.AutoSize = true;
+            this.tempLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tempLabel.Font = new System.Drawing.Font("Lucida Sans", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempLabel.ForeColor = System.Drawing.Color.White;
+            this.tempLabel.Location = new System.Drawing.Point(43, 66);
+            this.tempLabel.Name = "tempLabel";
+            this.tempLabel.Size = new System.Drawing.Size(138, 109);
+            this.tempLabel.TabIndex = 26;
+            this.tempLabel.Text = " 8";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.BackColor = System.Drawing.Color.Transparent;
+            this.dateLabel.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.ForeColor = System.Drawing.Color.White;
+            this.dateLabel.Location = new System.Drawing.Point(98, 202);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(37, 15);
+            this.dateLabel.TabIndex = 44;
+            this.dateLabel.Text = "Date";
+            // 
+            // conditionLabel
+            // 
+            this.conditionLabel.AutoSize = true;
+            this.conditionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.conditionLabel.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conditionLabel.ForeColor = System.Drawing.Color.White;
+            this.conditionLabel.Location = new System.Drawing.Point(67, 44);
+            this.conditionLabel.Name = "conditionLabel";
+            this.conditionLabel.Size = new System.Drawing.Size(103, 22);
+            this.conditionLabel.TabIndex = 46;
+            this.conditionLabel.Text = "Condition";
+            this.conditionLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // CurrentScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.conditionLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.cityOutput);
+            this.Controls.Add(this.maxLabel);
+            this.Controls.Add(this.minLabel);
+            this.Controls.Add(this.tempLabel);
+            this.Name = "CurrentScreen";
+            this.Size = new System.Drawing.Size(250, 400);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label cityOutput;
+        private System.Windows.Forms.Label maxLabel;
+        private System.Windows.Forms.Label minLabel;
+        private System.Windows.Forms.Label tempLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label conditionLabel;
+    }
+}
---
+++ XMLWeather/CurrentScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XMLWeather
+{
+    public partial class CurrentScreen : UserControl
+    {
+        public CurrentScreen()
+        {
+            InitializeComponent();
+            DisplayCurrent();
+        }
+
+        public void DisplayCurrent()
+        {
+            cityOutput.Text = Form1.days[0].location;
+            dateLabel.Text = Form1.days[0].date;
+            tempLabel.Text = Form1.days[0].currentTemp;
+            conditionLabel.Text = Form1.days[0].condition;
+            maxLabel.Text = Form1.days[0].tempHigh;
+            minLabel.Text = Form1.days[0].tempLow;
+
+        }
+    }
+}
